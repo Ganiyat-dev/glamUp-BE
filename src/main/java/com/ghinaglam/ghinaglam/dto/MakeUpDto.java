d98171a2afa6aa748f7cb1be6be440c777c58e8a
Ganiyat-dev <ganiyatolajumoke.go@gmail.com>
2022-11-06 08:00:47 +0100

    chore: Implement CRUD endpoint for Makeup Artist

--- src/main/java/com/ghinaglam/ghinaglam/dto/MakeUpDto.java
+++ src/main/java/com/ghinaglam/ghinaglam/dto/MakeUpDto.java
@@ -1,15 +1,17 @@
 package com.ghinaglam.ghinaglam.dto;
 
 import com.ghinaglam.ghinaglam.model.BaseEntity;
-import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
+
+import javax.validation.constraints.Email;
 
 @Getter
 @Setter
 public class MakeUpDto extends BaseEntity {
     private String firstName;
     private String lastName;
+    @Email
     private String email;
     //    @Column(unique = true)
     private String phoneNumber;
