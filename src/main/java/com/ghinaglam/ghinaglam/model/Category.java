48dee202178134ba4b999bc5da951bdfaad9f59b
Ganiyat-dev <ganiyatolajumoke.go@gmail.com>
2022-11-09 12:46:54 +0100

    update

--- src/main/java/com/ghinaglam/ghinaglam/model/Category.java
+++ src/main/java/com/ghinaglam/ghinaglam/model/Category.java
@@ -1,14 +1,6 @@
-//package com.ghinaglam.ghinaglam.model;
-//
-//import lombok.*;
-//
-//import javax.persistence.Entity;
-//
-//@Entity
-//@Getter
-//@Setter
-//@AllArgsConstructor
-//@NoArgsConstructor
-//public class Category extends BaseEntity {
-//    private String name;
-//}
+package com.ghinaglam.ghinaglam.model;
+
+public enum Category {
+    CLIENT, MAKEUP_ARTIST
+
+}
